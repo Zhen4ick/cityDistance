880f25038dd84378dc6dbceb06cd960c822fcdc9
Zhen4ick <45498896+Zhen4ick@users.noreply.github.com>
2019-03-26 21:23:10 +0300

    Added logic

--- Program.cs
+++ Program.cs
@@ -24,14 +24,14 @@ namespace cityDistance
             public float y;
             public string name;
        }
-        //Для инициализации массива
+       
        public static void initializeArray()
        {
             for(int i = 0; i < cities.Length; i++)
             {
                 cities[i] = new City();
             }       
-            //Координаты
+            //Coordinates y
             cities[0].y = 30.335f;//Saint-Petersburg           
             cities[1].y = 139.731f;//Tokyo
             cities[2].y = 37.617f;//Moscow
@@ -42,7 +42,7 @@ namespace cityDistance
             cities[7].y = -99.133f;//Mexico
             cities[8].y = 31.235f;//Kair
             cities[9].y = 103.819f;//Singapur
-
+            //Coordinates x
             cities[0].x = 59.934f;//Saint-Petersburg 
             cities[1].x = 35.709f;//Tokyo             
             cities[2].x = 55.755f;//Moscow
@@ -52,9 +52,8 @@ namespace cityDistance
             cities[6].x = 40.712f;//New York
             cities[7].x = 19.432f;//Mexico
             cities[8].x = 30.044f;//Kair
-            cities[9].x = 1.352f;//Singapur             
-                                 
-            //Имена городов
+            cities[9].x = 1.352f;//Singapur                                             
+            //Names of the ctites
             cities[0].name = "Saint-Petersburg";
             cities[1].name = "Tokyo";
             cities[2].name = "Moscow";
@@ -70,12 +69,80 @@ namespace cityDistance
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(FiggleFonts.Standard.Render("                                cityDistance"));
-            Console.WriteLine("0.Saint-Petersburg\t1.Tokyo\t2.Moscow\t3.Bejing\t4.London\t5.Paris\n6.New York\t        7.Mexico\t        " +
-           "8.Kair\t        9.Singapur");
-            string choice = Console.ReadLine();
+            Console.WriteLine("0.Saint-Petersburg\t1.Tokyo\t                2.Moscow\t3.Bejing\t4.London\t\n5.Paris                 6.New York\t        7.Mexico\t8.Kair\t        9.Singapur");
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            string choice;
+            int index1 = 0;
+            int index2 = 0;
+            float c;
+            float c2;
+            double result;
+            int result2;
             while (true)
             {
-                //Логику сюды
+                Console.WriteLine("Please select the number of first city");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                choice = Console.ReadLine();
+                for(int i = 0; i < cities.Length; i++)
+                {
+                    
+                    if (choice == i.ToString())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"You selected {cities[i].name}");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Coordinates:{cities[i].x}-x and {cities[i].y}-y");
+                        Console.WriteLine("\nPlease select the number of second city");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        index1 = i;
+                        choice = Console.ReadLine();
+                        break;
+                    }
+                  
+                   
+                    
+                        
+                    
+                }
+                for (int j = 0; j < cities.Length; j++)
+                {
+                    if(choice == j.ToString())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"You selected {cities[j].name}");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Coordinates:{cities[j].x}-x and {cities[j].y}-y");                        
+                        index2 = j;                       
+                        break;
+
+                    }
+                   
+                   
+                }
+                Console.WriteLine();
+                c = (cities[index2].x - cities[index1].x) * (cities[index2].x - cities[index1].x);
+                c2 = (cities[index2].y - cities[index1].y) * (cities[index2].y - cities[index1].y);
+                result = Convert.ToDouble(Math.Sqrt(c + c2));
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($@"The distance between {cities[index1].name} and {cities[index2].name} is {result}");
+                result2 = Convert.ToInt32(Math.Truncate(result));
+                result2 *= 100;
+                Console.WriteLine($@"This is about {result2} km");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Do you want to  continue[y/n]");
+                choice = Console.ReadLine();
+                if (choice == "y"||choice == "Y")
+                {
+                    continue;
+
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+                
+                
             }
             
         }
